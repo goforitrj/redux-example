d7821cba2b7a639d4161aa5272dae73877a0e156
goforitrj <eleveniasjr@gmail.com>
2020-02-11 19:18:58 +1100

    Refactoring for better performance

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import Counter from '../components/Counter';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import { increase, decrease, setDiff } from '../modules/counter';
 
 export default function CounterContainer() {
-    const { number, diff } = useSelector(state => ({
-        number: state.counter.number,
-        diff: state.counter.diff
-    }));
+    // useSelector 1)
+    // bad at perfornamce
+    // reason: rerender and create new {number,diff} eveytime when state is changed
+    // const { number, diff } = useSelector(state => ({
+    //     number: state.counter.number,
+    //     diff: state.counter.diff
+    // }));
+
+    // useSelector 2)
+    // good at performance
+    // reason: rerender only when number is changed
+    // const number = useSelector(state => state.counter.number);
+    // reason: rerender only when diff is changed
+    // const diff = useSelector(state => state.counter.diff);
+
+    // useSelector 3)
+    // good at performance
+    // using shallowEqual
+    const { number, diff } = useSelector(
+        state => ({
+            number: state.counter.number,
+            diff: state.counter.diff
+        }),
+        shallowEqual
+    );
+
     const dispatch = useDispatch();
 
     const onIncrease = () => {
